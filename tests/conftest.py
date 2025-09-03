8f52810d67a619b4d085a5cfaa47d5b152a2518f
Bigmafusail <rudov.dmitriy@gmail.com>
2025-09-03 11:29:32 +0400

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,52 @@ def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
     browser.close()
+
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright) -> None:
+    """Фикстура для регистрации пользователя и сохранения состояния браузера"""
+    # Запускаем Chromium браузер в обычном режиме (не headless)
+    browser = playwright.chromium.launch(headless=False)
+    # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
+    context = browser.new_context()
+    # Открываем новую страницу в рамках контекста
+    page = context.new_page()
+
+    # Открываем страницу регистрации
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    # Заполняем форму регистрации
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    # Нажимаем на кнопку "Registration"
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+    context.storage_state(path="browser-state.json")
+
+    # Закрываем браузер
+    browser.close()
+
+
+@pytest.fixture
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    """Фикстура для создания страницы с сохраненным состоянием браузера"""
+    # Создаем новую сессию браузера
+    browser = playwright.chromium.launch(headless=False)
+    # В контекст подставляем сохраненное состояние
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+
+    yield page
+
+    # Закрываем браузер после использования
+    browser.close()
