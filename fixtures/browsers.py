2532f3bad63efcba96f7760b30978c68f924a740
Bigmafusail <rudov.dmitriy@gmail.com>
2025-09-10 17:38:10 +0400

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -10,19 +10,13 @@ def chromium_page(playwright: Playwright) -> Page:
 
 
 @pytest.fixture(scope="session")
-def initialize_browser_state(playwright: Playwright) -> None:
-    """Фикстура для регистрации пользователя и сохранения состояния браузера"""
-    # Запускаем Chromium браузер в обычном режиме (не headless)
+def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
-    # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
     context = browser.new_context()
-    # Открываем новую страницу в рамках контекста
     page = context.new_page()
 
-    # Открываем страницу регистрации
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    # Заполняем форму регистрации
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
 
@@ -32,27 +26,16 @@ def initialize_browser_state(playwright: Playwright) -> None:
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
     password_input.fill('password')
 
-    # Нажимаем на кнопку "Registration"
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
     context.storage_state(path="browser-state.json")
-
-    # Закрываем браузер
     browser.close()
 
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
-    """Фикстура для создания страницы с сохраненным состоянием браузера"""
-    # Создаем новую сессию браузера
     browser = playwright.chromium.launch(headless=False)
-    # В контекст подставляем сохраненное состояние
     context = browser.new_context(storage_state="browser-state.json")
-    page = context.new_page()
-
-    yield page
-
-    # Закрываем браузер после использования
+    yield context.new_page()
     browser.close()
