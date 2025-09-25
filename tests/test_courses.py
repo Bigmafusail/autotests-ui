96c22dcb127dc764092b64c2c1fcd75d88075a2b
Bigmafusail <rudov.dmitriy@gmail.com>
2025-09-25 19:07:38 +0400

    Знакомство с PageComponent. Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -3,36 +3,17 @@ from playwright.sync_api import expect
 
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
-from pages.dashboard_page import DashboardPage
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
-    """Тест для проверки пустого списка курсов"""
-    page = chromium_page_with_state
-
-    # Открываем страницу курсов
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-    # Проверяем наличие и текст заголовка "Courses"
-    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_title).to_be_visible()
-    expect(courses_title).to_have_text("Courses")
-
-    # Проверяем иконку
-    courses_icon = page.get_by_test_id('courses-list-empty-view-icon')
-    expect(courses_icon).to_be_visible()
-
-    # Проверяем наличие и текст блока "There is no results"
-    no_results_block = page.get_by_test_id('courses-list-empty-view-title-text')
-    expect(no_results_block).to_be_visible()
-    expect(no_results_block).to_have_text("There is no results")
-
-    # Проверяем наличие и текст блока "Results from the load test pipeline will be displayed here"
-    no_results_description = page.get_by_test_id('courses-list-empty-view-description-text')
-    expect(no_results_description).to_be_visible()
-    expect(no_results_description).to_have_text("Results from the load test pipeline will be displayed here")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
 
 @pytest.mark.courses
