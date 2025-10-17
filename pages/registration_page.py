6ccd3550082e315989869f77e8aa9fcd19ae3cc0
Bigmafusail <rudov.dmitriy@gmail.com>
2025-10-17 10:22:22 +0400

    Знакомство с PageFactory. Практика работы с Page Factory

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,7 +1,10 @@
 from playwright.sync_api import Page, expect
 
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.button import Button
+from elements.link import Link
 from pages.base_page import BasePage
+from elements.text import Text
 
 
 class RegistrationPage(BasePage):
@@ -10,9 +13,9 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
 
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
-        self.login_link = page.get_by_test_id('registration-page-login-link')
-        self.wrong_user_already_exists_alert = page.get_by_test_id('registration-page-user-already-exists-alert')
+        self.registration_button = Button(page, 'registration-page-registration-button', "Registration")
+        self.login_link = Link(page, 'registration-page-login-link', "Login")
+        self.wrong_user_already_exists_alert = Text(page, 'registration-page-user-already-exists-alert', "Wrong User")
 
     def click_registration_button(self):
         self.registration_button.click()
@@ -21,5 +24,5 @@ class RegistrationPage(BasePage):
         self.login_link.click()
 
     def check_visible_wrong_user_already_exists_alert(self):
-        expect(self.wrong_user_already_exists_alert).to_be_visible()
-        expect(self.wrong_user_already_exists_alert).to_have_text("User already exists")
+        self.wrong_user_already_exists_alert.check_visible()
+        self.wrong_user_already_exists_alert.check_have_text("User already exists")
