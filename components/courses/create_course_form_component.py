32e48e92b1bf4093b0bbff958dd04c62366c8880
Bigmafusail <rudov.dmitriy@gmail.com>
2025-11-18 17:42:02 +0400

    Добавление Allure шагов в автотесты

--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -34,7 +34,7 @@ class CreateCourseFormComponent(BaseComponent):
         self.estimated_time_input.check_have_value(estimated_time)
 
         self.description_textarea.check_visible()
-        self.description_textarea.check_value(description)
+        self.description_textarea.check_have_value(description)
 
         self.max_score_input.check_visible()
         self.max_score_input.check_have_value(max_score)
